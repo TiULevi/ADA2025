802fb9b7291995edceac33722ee2360372344540
Levi Warren <leviwarren@NB51.obelink.local>
2025-03-22 09:43:16 +0100

    Sync local folder with GitHub repository

--- lab3/pubsubclients/consumer.py
+++ lab3/pubsubclients/consumer.py
@@ -42,5 +42,5 @@ def callback(message):
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    create_subscription("your project id", "diabetes_res", "diabetes_res_sub")
-    pull_message(project="your project id", subscription="diabetes_res_sub")  # replace your project id
+    create_subscription("ada2024-450119", "diabetes_res", "diabetes_res_sub")
+    pull_message(project="ada2024-450119", subscription="diabetes_res_sub")  # replace your project id
